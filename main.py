264a88930f49103c40b8881b7d84385ee1e83200
SheyonFN <83003954+SheyonFN@users.noreply.github.com>
2021-04-22 09:43:59 +0100

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,6 @@
+def on_forever():
+    pass
+basic.forever(on_forever)
+def on_logo_event_pressed():
+    pass
+input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_event_pressed)
---
+++ main.ts
@@ -0,0 +1,33 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("guess the number ")
+    basic.showNumber(randint(1, 10))
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("guess the number")
+    basic.showNumber(randint(0, 6))
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("guess the number ")
+    basic.showNumber(randint(1, 8))
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        . # # # .
+        # . . . #
+        `)
+})
